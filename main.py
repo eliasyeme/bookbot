a78e38986f08d682b7d0c8dc11fbd0ebf4f801db
eliasyeme <eliasyeme@gmail.com>
2024-02-09 08:46:17 +0300

    Add word and letter counting functions to main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+def count_words(text):
+    return len(text.split())
+
+
+def count_letters(text):
+    counted = {}
+    for letter in text:
+        key = letter.lower()
+        val = counted.get(key, 0) + 1
+        counted[key] = val
+    return counted
+
+
+def main():
+    with open("books/frankenstein.txt", "r") as f:
+        file_content = f.read()
+        print(file_content)
+        print(count_words(file_content))
+        print(count_letters(file_content))
+
+
+main()
